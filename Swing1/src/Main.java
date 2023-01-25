b29c7edd8d382b7de880f8d681f86db9b8ee4074
aroc00 <aroc00@JFC-5WKK2J2>
2023-01-25 12:28:05 +0100

    3

--- Swing1/src/Main.java
+++ Swing1/src/Main.java
@@ -1,8 +1,11 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args) {
 		JFrame myWindow = new JFrame();
+		JButton jb = new JButton("Click me");
+		myWindow.add(jb);
 		myWindow.setSize(200, 200);
 		myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myWindow.setVisible(true);
