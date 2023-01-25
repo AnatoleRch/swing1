b7e24cbc18641d97dd42728d936499c1cbe86d52
aroc00 <aroc00@JFC-5WKK2J2>
2023-01-25 12:28:25 +0100

    3

--- Swing1/src/Main.java
+++ Swing1/src/Main.java
@@ -1,11 +1,21 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 public class Main {
 
 	public static void main(String[] args) {
 		JFrame myWindow = new JFrame();
 		JButton jb = new JButton("Click me");
-		myWindow.add(jb);
+		JButton jb2 = new JButton("Coucou");
+		JButton jb3 = new JButton("C'est un gros bouton");
+		JPanel jp = new JPanel();
+		jp.add(jb);
+		jp.add(jb2);
+		jp.add(jb3);
+		myWindow.add(jp);
+		jb.addActionListener((e) -> {
+			System.out.println("coucou");
+		});
 		myWindow.setSize(200, 200);
 		myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myWindow.setVisible(true);
